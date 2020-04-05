e4534b0b3315e29336d3a003f28b77b7304e5df2
Jonathan Mischuk <jonathanmischuk@Jonathans-MacBook-Pro.local>
2020-04-05 16:59:35 -0400

    error handling

--- src/hooks/useAddFilterState.tsx
+++ src/hooks/useAddFilterState.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 
 import { emitter } from '../emitter';
 import { FeaturizeContext } from '../components';
@@ -7,6 +7,9 @@ export const useAddFilterState = () => {
 	const { instance } = useContext(FeaturizeContext);
 
 	return (filter: string, state: string): void => {
+		if (!instance.hasFilter(filter))
+			throw instance.Featurize.errorFilterDoesNotExist(filter);
+
 		if (!instance.hasFilterState(filter, state)) {
 			instance.addFilterState(filter, state);
 			emitter.emit('update');
